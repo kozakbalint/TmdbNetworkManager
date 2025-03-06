8d3b55417e6216ec2be642d2f638801db5a0a0e8
Bálint Kozák <balint.kozak@capgemini.com>
2025-03-06 12:42:55 +0100

    Expose public initializers for useful structs

--- Sources/TmdbNetworkManager/Models/MovieDetailsResponse.swift
+++ Sources/TmdbNetworkManager/Models/MovieDetailsResponse.swift
@@ -29,27 +29,86 @@ public struct MovieDetailsResponse: Decodable, Identifiable {
     public let video: Bool
     public let voteAverage: Double
     public let voteCount: Int
+    
+    public init(adult: Bool, backdropPath: String?, belongsToCollection: BelongsToCollection?, budget: Int, genres: [Genre]?, homepage: String?, id: Int, imdbId: String?, originCountry: [String]?, originalLanguage: String?, originalTitle: String?, overview: String?, popularity: Double, posterPath: String?, productionCompanies: [ProductionCompany]?, productionCountries: [ProductionCountry]?, releaseDate: String?, revenue: Int, runtime: Int, spokenLanguages: [SpokenLanguage]?, status: String?, tagline: String?, title: String?, video: Bool, voteAverage: Double, voteCount: Int) {
+        self.adult = adult
+        self.backdropPath = backdropPath
+        self.belongsToCollection = belongsToCollection
+        self.budget = budget
+        self.genres = genres
+        self.homepage = homepage
+        self.id = id
+        self.imdbId = imdbId
+        self.originCountry = originCountry
+        self.originalLanguage = originalLanguage
+        self.originalTitle = originalTitle
+        self.overview = overview
+        self.popularity = popularity
+        self.posterPath = posterPath
+        self.productionCompanies = productionCompanies
+        self.productionCountries = productionCountries
+        self.releaseDate = releaseDate
+        self.revenue = revenue
+        self.runtime = runtime
+        self.spokenLanguages = spokenLanguages
+        self.status = status
+        self.tagline = tagline
+        self.title = title
+        self.video = video
+        self.voteAverage = voteAverage
+        self.voteCount = voteCount
+    }
 }
 
 public struct BelongsToCollection: Decodable {
     public let id: Int
     public let name, posterPath, backdropPath: String?
+    
+    public init(id: Int, name: String?, posterPath: String?, backdropPath: String?) {
+        self.id = id
+        self.name = name
+        self.posterPath = posterPath
+        self.backdropPath = backdropPath
+    }
 }
 
 public struct Genre: Decodable {
     public let id: Int
     public let name: String?
+    
+    public init(id: Int, name: String?) {
+        self.id = id
+        self.name = name
+    }
 }
 
 public struct ProductionCompany: Decodable {
     public let id: Int
     public let logoPath, name, originCountry: String?
+    
+    public init(id: Int, logoPath: String?, name: String?, originCountry: String?) {
+        self.id = id
+        self.logoPath = logoPath
+        self.name = name
+        self.originCountry = originCountry
+    }
 }
 
 public struct ProductionCountry: Decodable {
     public let iso31661, name: String?
+    
+    public init(iso31661: String?, name: String?) {
+        self.iso31661 = iso31661
+        self.name = name
+    }
 }
 
 public struct SpokenLanguage: Decodable {
     public let englishName, iso6391, name: String?
+    
+    public init(englishName: String?, iso6391: String?, name: String?) {
+        self.englishName = englishName
+        self.iso6391 = iso6391
+        self.name = name
+    }
 }
--- Sources/TmdbNetworkManager/Models/MovieResponse.swift
+++ Sources/TmdbNetworkManager/Models/MovieResponse.swift
@@ -20,4 +20,21 @@ public struct MovieResponse: Decodable, Identifiable {
     public let video: Bool
     public let voteAverage: Double
     public let voteCount: Int
+    
+    public init(adult: Bool, backdropPath: String?, genreIds: [Int], id: Int, originalLanguage: String?, originalTitle: String?, overview: String?, popularity: Double, posterPath: String?, releaseDate: String?, title: String?, video: Bool, voteAverage: Double, voteCount: Int) {
+        self.adult = adult
+        self.backdropPath = backdropPath
+        self.genreIds = genreIds
+        self.id = id
+        self.originalLanguage = originalLanguage
+        self.originalTitle = originalTitle
+        self.overview = overview
+        self.popularity = popularity
+        self.posterPath = posterPath
+        self.releaseDate = releaseDate
+        self.title = title
+        self.video = video
+        self.voteAverage = voteAverage
+        self.voteCount = voteCount
+    }
 }
